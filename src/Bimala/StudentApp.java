900182bd916acf0fe9f1811d984bd7ebd2817ab5
gedilabimalabsc24-code <gedilabimalabsc24@rvu.edu.in>
2025-11-10 07:06:19 +0530

    Update StudentApp.java
    Updated StudentApp

---
+++ src/Bimala/StudentApp.java
@@ -0,0 +1,15 @@
+/*
+ * StudentApp.java - Launches the Student Management JavaFX Application
+ * Author: Bimala
+ * Date: November 2025
+ */
+
+package Bimala;
+
+import javafx.application.Application;
+
+public class StudentApp {
+    public static void main(String[] args) {
+        Application.launch(MainWindow.class, args);
+    }
+}
